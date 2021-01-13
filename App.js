4d04cadfa7949ef289192dc0f7c32ae04f9334f2
Larry Nxumalo <larry.nxumalo@afrihost.com>
2021-01-13 20:53:56 +0200

    initialising firebase for gram-app

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as firebase from 'firebase';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAVFbuyz3kdWj8avUd8sDI7lbP1KmBRqjI",
+  authDomain: "gram-grem.firebaseapp.com",
+  projectId: "gram-grem",
+  storageBucket: "gram-grem.appspot.com",
+  messagingSenderId: "243184121628",
+  appId: "1:243184121628:web:f37ad7d7c7bbe7bd896cd9",
+  measurementId: "G-06JF4CEKSH"
+};
+
+if(firebase.apps.length === 0){
+  firebase.initializeApp(firebaseConfig)
+}
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import LandingScreen from './components/auth/Landing'
+import RegisterScreen from './components/auth/Register'
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Landing">
+        <Stack.Screen name="Landing" component={LandingScreen} options={{ headerShown: false}}/>
+        <Stack.Screen name="Register" component={RegisterScreen} options={{ headerShown: true}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
