370f6302599319f859b568dd8c47371e757afadd
Larry Nxumalo <larry.nxumalo@afrihost.com>
2021-01-21 17:10:44 +0200

    showing logged on view for logged in user

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
+
+import { View, Text } from 'react-native';
 import * as firebase from 'firebase';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -17,6 +19,8 @@ if(firebase.apps.length === 0){
   firebase.initializeApp(firebaseConfig)
 }
 
+
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -24,15 +28,55 @@ import LandingScreen from './components/auth/Landing'
 import RegisterScreen from './components/auth/Register'
 
 const Stack = createStackNavigator();
+export class App extends Component {
+  constructor(props){
+      super(props);
+       this.state = {
+         loaded: false,
+       }
+  }
+  componentDidMount(){
+      firebase.auth().onAuthStateChanged((user) => {
+        if(!user){
+          this.setState({
+            loggedIn: false,
+            loaded: true,
+          })
+        }else {
+          this.setState({
+            loggedIn: true,
+            loaded: true,
+        })}
+      })
+  }
+  render() {
+    const { loggedIn, loaded } = this.state;
+    if(!loaded){
+      return(
+          <View style={{ flex: 1, justifyContent: 'center' }}>
+            <Text>Loading...</Text>
+          </View>
+      )
+    }
+    if(!loggedIn){
+      return (
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Landing">
+            <Stack.Screen name="Landing" component={LandingScreen} options={{ headerShown: false}}/>
+            <Stack.Screen name="Register" component={RegisterScreen} options={{ headerShown: true}}/>
+          </Stack.Navigator>
+        </NavigationContainer>
+      );
+    }
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Landing">
-        <Stack.Screen name="Landing" component={LandingScreen} options={{ headerShown: false}}/>
-        <Stack.Screen name="Register" component={RegisterScreen} options={{ headerShown: true}}/>
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    return(
+      <View style={{ flex: 1, justifyContent: 'center' }}>
+        <Text>You're Within!</Text>
+      </View>
+  )
+  }
 }
+
+export default App
+
 
